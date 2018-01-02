364ecfb6317727addd110a332f1f831a9f37c4c0
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2018-01-02 16:14:03 +0800

    update

--- index.js
+++ index.js
@@ -1,54 +1,45 @@
 'use strict';
 
-var _express = require('express');
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-var _express2 = _interopRequireDefault(_express);
-
-var _bluebird = require('bluebird');
-
-var _bluebird2 = _interopRequireDefault(_bluebird);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var line = require('@line/bot-sdk');
-
-
-var config = {
-	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-	channelSecret: process.env.CHANNEL_SECRET
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-var client = new line.Client(config);
+// create LINE SDK client
+const client = new line.Client(config);
 
-var app = (0, _express2.default)();
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
 
-var handleEvent = function handleEvent(event) {
-	console.log(event);
-
-	if (event.type !== 'message' || event.message.type !== 'text') {
-		// ignore non-text-message event
-		return _bluebird2.default.resolve(null);
-	}
-
-	// create a echoing text message
-	var echo = { type: 'text', text: event.message.text };
-
-	// use reply API
-	return client.replyMessage(event.replyToken, echo);
-};
-
-app.post('/callback', line.middleware(config), function (req, res) {
-	_bluebird2.default.all(req.body.events.map(handleEvent)).then(function (result) {
-		console.log(result);
-		res.end();
-	}).catch(function (err) {
-		console.log(err);
-		res.status(500).end();
-	});
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
 
-var port = process.env.PORT || 3000;
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
 
-app.listen(port, function () {
-	console.log('listening on port:' + port);
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
